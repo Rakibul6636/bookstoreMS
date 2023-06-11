97fcba7185d6ee5cb0bc2ff51e3370a2cfbefa57
Rakibul6636 <rakib.cse.bru@gmail.com>
2023-06-11 14:55:57 +0600

    Update BookController.java
    added home

--- bookstoreMS/src/main/java/com/dhrubok/bookstoreMS/controller/BookController.java
+++ bookstoreMS/src/main/java/com/dhrubok/bookstoreMS/controller/BookController.java
@@ -24,7 +24,7 @@ public class BookController {
 	@Autowired
 	private AuthorRepository aRepo;
 	
-	@GetMapping({"/test"})
+	@GetMapping({"/"})
 	public ModelAndView testMethod() {
 		System.out.println("inside list");
 		ModelAndView mav = new ModelAndView("app-layout");
